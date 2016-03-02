992270028a923961cc2b607039e278bc1c3c4c91
colemonnahan <monnahc@uw.edu>
2016-03-02 11:24:16 -0800

    Update case files to be able to write determinstic ones

--- startup.R
+++ startup.R
@@ -32,31 +32,29 @@ message(paste(getDoParWorkers(), "cores have been registered for",
 message("Writing case files...")
 
 ### ------------------------------------------------------------
-write_cases_data <- function(case, spp, Nsamp, ESS.scalar, dir=case_folder){
-    ## Years need to be different for different species
-    if(spp=='yellow-long'){
-        ## just add 75 more years due to the extra burn in
-        index.years1 <- 'years;list(75+seq(76,100, by=2))'
-        index.years2 <- 'years;list(75+c(seq(94,100,by=2)))'
-        comp.years1 <- 'years;list(75+c(36,46,seq(51,66,by=5),71:100), 75+seq(76,100, by=2))'
-        comp.years2 <- 'years;list(75+c(seq(86,90,by=10), 91:100), 75+seq(94,100,by=2))'
-    } else {
+write_cases_data <- function(case, spp, Nsamp, survey.cv, ESS.scalar, dir=case_folder,
+                             deterministic){
+    ## The main data cases
+    if(!deterministic){
         index.years1 <- 'years;list(seq(76,100, by=2))'
         index.years2 <- 'years;list(c(seq(94,100,by=2)))'
         comp.years1 <- 'years;list(c(36,46,seq(51,66,by=5),71:100), seq(76,100, by=2))'
         comp.years2 <- 'years;list(c(seq(86,90,by=10), 91:100), seq(94,100,by=2))'
+    } else {
+        ## determinstic runs for checks during development
+        index.years1 <- index.years2 <- comp.years1 <- comp.years2 <-
+            'years;list(1:100, 1:100)'
     }
     Nsamp.string <- paste0('Nsamp;list(', paste0(Nsamp, collapse=','), ')')
     ESS.string <- paste0('ESS;list(', paste0(Nsamp*ESS.scalar, collapse=','), ')')
     ## data rich w/ age comps
-    index <- c('fleets;c(2)', index.years1 , 'sds_obs;list(.2)')
+    index <- c('fleets;c(2)', index.years1 , paste0('sds_obs;list(',survey.cv,'))'))
     lcomp <- c('fleets;c(1,2)', comp.years1, Nsamp.string, ESS.string,  'cpar;c(NA,NA)')
     agecomp <- lcomp
     writeLines(index, con=paste0(dir,"/", "index", case,"-", spp, ".txt"))
     writeLines(lcomp, con=paste0(dir,"/", "lcomp", case,"-", spp, ".txt"))
     writeLines(agecomp, con=paste0(dir,"/", "agecomp", case,"-", spp, ".txt"))
 }
-
 write_cases_selex <- function(spp, case, scalar, dir=case_folder){
     ## The true parameter value for P1 for fishery is 50.8
     ## set.seed(1)
@@ -134,9 +132,11 @@ for(spp in species) {
     ## Get the F cases from the package since based on Fmsy
     file.copy(from=paste0("models/F1-", spp,'.txt'), to=case_folder)
     ## write the data and binning cases
-    trash <- sapply(D.cases, function(i)
-        write_cases_data(case=i, spp=spp, Nsamp=c(100,500),
-                         ESS.scalar=ESS.scalar.vec[i]))
+    trash <- sapply(D.cases, function(i){
+        write_cases_data(case=i, spp=spp, Nsamp=c(100,500), survey.cv=.2,
+                         ESS.scalar=ESS.scalar.vec[i], deterministic=FALSE)
+        write_cases_data(case=100+i, spp=spp, Nsamp=c(5000,5000), survey.cv=.01,
+                         ESS.scalar=ESS.scalar.vec[i], deterministic=TRUE)})
     trash <- sapply(S.cases, function(i)
         write_cases_selex(spp=spp, case=i, scalar=selex.scalar.vec[i]))
     trash <- sapply(E.cases, function(i)  write_cases_estimation(spp, case=i))
